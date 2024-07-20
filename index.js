cf58da8f141e6c26226a3258af383ca46da4e562
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-07-20 10:08:28 -0500

    commented out secure cors, due to loading issues.

--- index.js
+++ index.js
@@ -30,27 +30,28 @@ app.use(express.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-let allowedOrigins = [
-  'https://natesmovieflix-742bdbb68d51.herokuapp.com/',
-  'http://localhost:3000',
-  'http://localhost:1234',
-  'http://testsite.com',
-];
+// let allowedOrigins = [
+//   'https://natesmovieflix-742bdbb68d51.herokuapp.com/',
+//   'http://localhost:3000',
+//   'http://localhost:1234',
+//   'http://testsite.com',
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application doesn't allow acces from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app
+  .use
+  // cors({
+  //   origin: (origin, callback) => {
+  //     if (!origin) return callback(null, true);
+  //     if (allowedOrigins.indexOf(origin) === -1) {
+  //       let message =
+  //         "The CORS policy for this application doesn't allow acces from origin " +
+  //         origin;
+  //       return callback(new Error(message), false);
+  //     }
+  //     return callback(null, true);
+  //   },
+  // })
+  ();
 
 let auth = require('./auth')(app);
 const passport = require('passport');
