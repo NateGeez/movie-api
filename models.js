45eb51f102064d852e4d52c2649a223640792ba9
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-08-05 19:46:44 -0500

    syntax correction

--- models.js
+++ models.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-  Title: { type: String, require: true },
-  Description: { type: String, require: true },
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
   Genre: {
     Name: String,
     Description: String,
