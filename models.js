e13ceb3415fb5236933db08b3c29a2a3407e63fd
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-08-07 09:38:03 -0500

    accepting plain text passwords for existing user database

--- models.js
+++ models.js
@@ -30,8 +30,18 @@ userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
+// Password hashing occurs during user creation or updating
+// User database was created before password hashing was incorporated
+// Therefore I added return plain text password for existing user database
 userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.Password);
+  // Check if the stored password is hashed
+  const isPasswordHashed = this.Password.startsWith('$2b$');
+  if (isPasswordHashed) {
+    return bcrypt.compareSync(password, this.Password);
+  } else {
+    // For plain text passwords, compare directly
+    return password === this.Password;
+  }
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
